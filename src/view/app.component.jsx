96376d0f14a0b98ae3e1a67d648dd8cc23c4a6fc
maxvasiukov <vasyukovmo@yandex.ru>
2023-07-24 14:20:37 +0400

    Added app component and has been reset browser styles

---
+++ src/view/app.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './app.css';
+import ProductsChart from './products-chart/products-chart.component';
+
+export default function App() {
+  return <ProductsChart />;
+}
